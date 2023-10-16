4b10de43e9bea1627af532c0d85518bd600af68a
nazmiyegnydnn <nazmiyeyilmaz159@gmail.com>
2023-10-16 23:16:56 +0300

    Home page design started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.scss';
 
+
 function App() {
   return (
      <div className='home'>
+      <div className='homeMenu'>
       <div className='homeLeft'>
-      <h2>KATEGORİLER</h2>
+      <h2>KİTAP TÜRLERİ</h2>
+      <ul>
+        <li><button>ROMAN</button></li>
+        <li><button>HİKAYE</button></li>
+        <li><button>ŞİİR</button></li>
+        <li><button>BİYOGRAFİ</button></li>
+      </ul>
       </div>
       <div className='homeRight'>
       <h1>MY TODO LİST</h1>
-      <input />
+      <input type='text' 
+            className='todo-input' 
+            placeholder='Add...'
+            // onChange={inputTextHandler}
+            // value={inputText}
+            />
+      <button className='add'>ADD</button>
+      <div className='list'>
+        <button className='completed'>Tamamlandı</button>
+        <label>Todo</label>
+        <button className='delete'>Sil</button>
+      </div>
+      </div>
       </div>
      </div>
   );
