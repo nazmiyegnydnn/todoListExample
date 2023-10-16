8749d3d792f1a5ca8af66a2349b69d49328a6ed7
Nazmiye Günaydın <107623762+nazmiyegnydnn@users.noreply.github.com>
2023-10-16 16:26:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+     <div className='home'>
+      <div className='homeLeft'>
+      <h2>KATEGORİLER</h2>
+      </div>
+      <div className='homeRight'>
+      <h1>MY TODO LİST</h1>
+      <input />
+      </div>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/appSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit' 
+
+const initialState = { 
+    product: [
+        {
+            id:1,
+            name: "tolga"
+        },
+        {
+            id:2,
+            name: "tolga"
+        }
+    ],
+}
+  
+
+  export const appSlice =  createSlice({ 
+    name: 'app',  
+    initialState,
+    reducers: {
+            // kull olay //
+             addcategory : (state , action)  => {
+            state.category = [...state.category , {id:3 , name: "c3"}]  
+        }
+        
+    }
+  })
+
+  export const {addcategory} = appSlice.actions  
+  export default appSlice.reducer 
