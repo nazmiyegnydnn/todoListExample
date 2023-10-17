835f724332e893bb8e45bc5854c18f3a0a1102b2
nazmiyegnydnn <nazmiyeyilmaz159@gmail.com>
2023-10-17 22:50:36 +0300

    The design part and the category creation part have been completed.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,82 @@
-import './App.scss';
+import Logo from "./logo.png";
+import { useEffect, useState } from "react";
+import "./App.scss";
 
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodos, setNewTodos] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("selectCategory"); 
 
-function App() {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((todos) => setTodos(todos.slice(0, 5)));
+  }, []);
+
+  const handleInput = (e) => {
+    setNewTodos(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const addItem = () => {
+    const newItem = {
+      id: todos.length + 1,
+      title: newTodos,
+      completed: false,
+      category: selectedCategory, 
+    };
+    setTodos([...todos, newItem]);
+    setNewTodos("");
+  };
+
   return (
-     <div className='home'>
-      <div className='homeMenu'>
-      <div className='homeLeft'>
-      <h2>KİTAP TÜRLERİ</h2>
-      <ul>
-        <li><button>ROMAN</button></li>
-        <li><button>HİKAYE</button></li>
-        <li><button>ŞİİR</button></li>
-        <li><button>BİYOGRAFİ</button></li>
-      </ul>
+    <div className="home">
+      <div className="homeHeader">
+        <img src={Logo} alt="Logo" />
+        <h1>To-do List</h1>
       </div>
-      <div className='homeRight'>
-      <h1>MY TODO LİST</h1>
-      <input type='text' 
-            className='todo-input' 
-            placeholder='Add...'
-            // onChange={inputTextHandler}
-            // value={inputText}
-            />
-      <button className='add'>ADD</button>
-      <div className='list'>
-        <button className='completed'>Tamamlandı</button>
-        <label>Todo</label>
-        <button className='delete'>Sil</button>
+      <div className="todoList">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Add..."
+          onChange={handleInput}
+          value={newTodos}
+        />
+        <div className="select">
+          <select
+            name="todos"
+            className="filter-todo"
+            value={selectedCategory} 
+            onChange={handleCategoryChange}
+          >
+            <option value="selectCategory" disabled selected>Kategori Seç</option>
+            <option value="important">Önemli</option>
+            <option value="very-important">Çok Önemli</option>
+            <option value="less-important">Az Önemli</option>
+          </select>
+          <button className="add" onClick={addItem}>
+            ADD
+          </button>
+        </div>
       </div>
+      <div className="todosCard">
+        {todos.map((el) => (
+          <div className="list" key={el.id}>
+            <span>{el.id}</span>
+            <p>{el.title}</p>
+            <span>{el.completed ? "Tamamlandı" : "Tamamlanmadı"}</span>
+            <span>Kategori: {el.category}</span>
+          </div>
+        ))}
       </div>
-      </div>
-     </div>
+    </div>
   );
-}
+};
 
 export default App;
